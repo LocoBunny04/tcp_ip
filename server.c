09fa2ed74ba702379c60dbfa4c3cc3d9327732d3
Kawinthida Haase <78894109+LocoBunny04@users.noreply.github.com>
2025-04-25 20:51:46 -0500

    Update server.c
    4/25/25 8:51pm

--- server.c
+++ server.c
@@ -27,19 +27,60 @@
 /* Global Variables */
 void cleanup(void);
 void signal_handler(int sig);
+void err_msg(char *);
 int ls_fd = -1; /* Listening socket descriptor */
 int rs_fd = -1; /* Receive socket descriptor */
 int outf_fd = -1; /* Output file descriptor */
 
 void cleanup(void) {
-    /* Proper release memory */
-    if (client)
-}
-void signal_handler(int sig) {
-    /* Issue an error */
-    
+    /* free buffer if needed */
+    if( in_buf != NULL ) {
+        free( in_buf );
+        in_buf = NULL;
+    }
+
+    /* close listening socket if needed */
+    if( ls_fd >= 0 ) {
+        close( ls_fd );
+        ls_fd = -1;
+    }
+
+    /* close receiving socket if needed */
+    if( rs_fd >= 0 ) {
+        close( rs_fd );
+        rs_fd = -1;
+    }
+
+    /* close output file if needed */
+    if( of_fd >= 0 ) {
+        close( of_fd );
+        of_fd = -1;
+    }
+
+    /* missing Goodbye! message */
+    printf( "server: Goodbye!\n" );
+
 }
 
+/* Signal handler for SIGINT (via [CTRL]+[C]) */
+void SIGINT_handler( int sig ) {
+
+    /* Maybe issue a message */
+    fprintf( stderr, "Server interrupted.  It will now be terminated.\n" );
+  
+    cleanup();
+ 
+    /* Terminate the program... for 'reals' this time. */
+    exit( EXIT_FAILURE );
+ 
+} /* end SIGINT_handler() */
+
+ /* Error message */
+void err_msg(char, *msg){
+    fprintf(stderr, msg);
+    cleanup();
+    exit(EXIT_FAILURE);
+ }
 /* Main*/
 
 int main(int argc, char *argv[]) {
@@ -53,7 +94,7 @@ int main(int argc, char *argv[]) {
     * register signal handler
     * create tcp socket
     * so_reuseaddr socket option
-    optional enable so_resuseport
+    * optional enable so_resuseport
     * initialize server address structure
     * retry binding if port is in use
     * listen on socket
@@ -61,9 +102,55 @@ int main(int argc, char *argv[]) {
     * receive data from client (generate uniq file name)
     * write data to file
     * allocate buffer for each file transfer individually
-    receive data from client
+    * receive data from client
     * check for receive errors
     * cleanup
-
     */
+
+    unsigned short port;
+    char *endptr;
+    int opt = 1;
+    /* listening socket internet address */
+    struct in_addr ls_ia;
+    /* listening socket socket address */
+    struct sockaddr_in ls_sa;
+    struct server_addr, client_addr;
+
+    if (argc != 2){
+        err_msg("server: Usage server <listen_Port>\n");
+    }
+
+    /* Port */
+    port = (unsigned short int) atoi(argv[1]); /* Parsing port input to a number using atoi to convert string to number */
+    /* Check if the input for the port number is valid and within the acceptable range */
+    if (*endptr != '\0' || port < 0 || port > 65535 || (port >= 0 && port <= 1023)) {
+        if (server_port < 0 || server_port > 65535) {
+            err_msg("server: Error: Invalid port number.\n");
+        } else {
+            err_msg("server: ERROR: Port number is privileged.\n");
+        }
+    }
+
+    /* signal handler */
+    signal(SIGINT, signal_handler);
+    signal(SIGPIPE, SIG_IGN); /* Ignore SIGPIPE to handle disconnections */
+
+    /* set up socket*/
+    ls_ia.s_addr = inet_addr( "127.0.0.1" );
+    ls_sa.sin_family = AF_INET;
+    ls_sa.sin_addr = ls_ia;
+    ls_sa.sin_port = htons( port );
+
+    /* Create TCP socket */
+    ls_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(ls_fd < 0){
+        perror("server: Error: Failed to create socket\n");
+        return(EXIT_FAILURE);
+    }
+
+    /* Making listening socket reusable */
+    if(setsockopt(ls_fd, SQL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <0){
+        err_msg("Server: ERROR: Failed to set SO_REUSEADDR option");
+    }
+    
 }
